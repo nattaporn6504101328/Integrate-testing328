054d6de92427b72ff3e8c1b0765fff0c09c0d4fd
nattaporn6504101328 <nattaporn6504101328@gmail.com>
2023-10-13 21:51:20 +0700

    first commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="htmlClass">
+    <h1>{{ title }}</h1>
+  </div>
+  <InputPage/>
+  <FunctionPage/>
+</template>
+
+
+<script>
+import InputPage from 'src/pages/InputPage.vue'
+import FunctionPage from './FunctionPage.vue';
+export default {
+  name: 'HtmlComponent',
+    components:{
+      InputPage,FunctionPage
+    },
+  data() {
+    return {
+      title: 'nattaporn wadrakchid',
+    }
+  },
+}
+</script>
+
+<style scoped></style>
